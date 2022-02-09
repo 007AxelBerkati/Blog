576ba95d41b0877609cb7bd66bd94ee8982712a6
007AxelBerkati <bucel1916@gmail.com>
2022-02-09 12:06:13 +0700

    Advance State Management With Context

---
+++ src/screens/IndexScreen.js
@@ -0,0 +1,57 @@
+import {
+  Button,
+  FlatList,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import React, { useContext } from "react";
+import { Context } from "../context/BlogContext";
+import { Feather } from "@expo/vector-icons";
+
+const indexScreen = () => {
+  const { state, addBlogPost } = useContext(Context);
+  return (
+    <View>
+      <Button title="Add Post" onPress={addBlogPost} />
+      <FlatList
+        data={state}
+        keyExtractor={(blogPosts) => blogPosts.id}
+        renderItem={({ item }) => {
+          return (
+            <View style={styles.row}>
+              <Text style={styles.title}>
+                {item.title} - {item.id}
+              </Text>
+              <TouchableOpacity>
+                <Feather style={styles.icon} name="trash" />
+              </TouchableOpacity>
+            </View>
+          );
+        }}
+      />
+    </View>
+  );
+};
+
+export default indexScreen;
+
+const styles = StyleSheet.create({
+  row: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingVertical: 20,
+    borderTopWidth: 1,
+    borderColor: "gray",
+    paddingHorizontal: 10,
+  },
+
+  title: {
+    fontSize: 18,
+  },
+
+  icon: {
+    fontSize: 24,
+  },
+});
